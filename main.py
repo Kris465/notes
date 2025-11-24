da93e24313e4d7b5c7573031b907fbe62fad67df
Kris465 <nataza7394@gmail.com>
2025-11-24 20:58:29 +0300

    notes

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+from loguru import logger
+
+from controller import Controller
+
+
+def main():
+    logger.remove(0)
+    logger.add(
+        "file.log",
+        level="INFO",
+        retention="3 days",
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
+    )
+    
+    controller = Controller()
+    controller.run()
+
+
+if __name__ == '__main__':
+    main()
